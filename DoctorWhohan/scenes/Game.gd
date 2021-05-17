e481656e33a8f3366a22563f23854dc9df973e58
Lukas-12 <lukas.buchsi@gmail.com>
2021-05-17 09:36:31 +0200

    Funktion und Layout für Leben
    Funktion, die die Anzahl an Leben überprüft implementiert + Leben Dummys Links oben im Eck

--- DoctorWhohan/scenes/Game.gd
+++ DoctorWhohan/scenes/Game.gd
@@ -2,16 +2,21 @@ extends Node
 
 
 export (PackedScene) var Enemy
-
+var score #Punktestand
+var life  #Leben 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$EnemyTimer.start()
+	score = 0
+	life = 3
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	checkLife()
+	
+
 
 
 func _on_EnemyTimer_timeout():
@@ -21,11 +26,35 @@ func _on_EnemyTimer_timeout():
 	var enemy = Enemy.instance()
 	add_child(enemy)
 	enemy.position = $EnemyPath/PathFollow2D.position
-	enemy.linear_velocity = Vector2(-50,0)
+	enemy.linear_velocity = Vector2(-100,0)
 	
 	
 	
 
 #Function die Aufgerufen wird, wenn Patient die hitbox des Doktors trifft
 func _on_Player_hit():
+	life = life -1
+	
+#Wird aufgerufen, wenn alle Leben verbraucht sind
+func gameOver():
 	$Player.hide()
+	
+#Funktion, die Anzahl der Leben überprüft 
+func checkLife():
+	if life == 3:
+		$LifeOne.show()
+		$LifeTwo.show()
+		$LifeThree.show()
+	elif life == 2:
+		$LifeOne.show()
+		$LifeTwo.show()
+		$LifeThree.hide()
+	elif life == 1:
+		$LifeOne.show()
+		$LifeTwo.hide()
+		$LifeThree.hide()
+	else:
+		$LifeOne.hide()
+		$LifeTwo.hide()
+		$LifeThree.hide()
+		gameOver()
